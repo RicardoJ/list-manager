6c149f901f1b68107b1a9931cdb95ee4be8cae44
Ricardo <rjdiazzh@gmail.com>
2020-08-15 14:46:49 -0500

    creating container and wrapper global

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
 import styled from "styled-components";
-const AppWrapper = styled.div`
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
   display: flex;
-  background: red;
-  
+  background-color: #e5e8e8;
 `;
+
+const AppWrapper = styled.div`
+  width: 90%;
+  max-width: 150rem;
+  height: 85vh;
+  margin: 50px auto;
+  border-radius: 0.5rem;
+  box-shadow: 0 1rem 5rem rgba(0, 0, 0, 0.2);
+`;
+
 function App() {
-  return <AppWrapper>Hi there</AppWrapper>;
+  return (
+    <Container>
+      <AppWrapper>Hi there</AppWrapper>
+    </Container>
+  );
 }
 
 export default App;
