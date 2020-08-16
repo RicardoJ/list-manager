411f65aa7afe27e66400aabe454eadbd8ed8e673
Ricardo <rjdiazzh@gmail.com>
2020-08-16 17:39:45 -0500

    button share

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,12 +3,10 @@ import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
-
+import { faShare } from "@fortawesome/free-solid-svg-icons";
 import Button from "./Button";
 
 const Container = styled.div`
-  font-size: 1.8rem;
-  letter-spacing: 0.1rem;
   margin-bottom: 1.5rem;
   padding: 0.4rem 0.4rem 0.4rem 0.7rem;
   border-radius: 0.3rem;
@@ -17,8 +15,13 @@ const Container = styled.div`
   justify-content: space-between;
   box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.2);
 `;
+const NameItem = styled.div`
+  font-size: 1.8rem;
+  letter-spacing: 0.1rem;
+`;
+
 const Icons = styled.div`
-  width: 5rem;
+  width: 10rem;
   display: flex;
   justify-content: space-between;
 `;
@@ -26,10 +29,13 @@ const Icons = styled.div`
 const Item = ({ item, onEdit, onRemoveItem, onItemClick }) => {
   return (
     <Container>
-      <Button onClick={() => onItemClick(item)}>{item.name}</Button>
+      <NameItem>{item.name}</NameItem>
       <Icons>
         <Button onClick={() => onEdit(item)}>
           <FontAwesomeIcon icon={faEdit} title="Edit" />
+        </Button>
+        <Button onClick={() => onItemClick(item)}>
+          <FontAwesomeIcon icon={faShare} title="Share" />
         </Button>
         <Button onClick={() => onRemoveItem(item.id)}>
           <FontAwesomeIcon icon={faTrash} title="Delete" />
